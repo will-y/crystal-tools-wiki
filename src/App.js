f8cc70c213d6309426ce01354b80c6ff3cb4c0b9
Will Yelton <willhyelton@gmail.com>
2022-03-18 11:21:58 -0400

    Edited labels for navbar

--- src/App.js
+++ src/App.js
@@ -28,21 +28,15 @@ function App() {
 
     return (
         <>
-            <Navbar bg="light" expand="lg">
-                <Container>
-                    <Navbar.Brand href="#home">React-Bootstrap</Navbar.Brand>
+            <Navbar bg="light" expand="sm">
+                <Container fluid>
+                    <Navbar.Brand href="#home">Crystal Tools</Navbar.Brand>
                     <Navbar.Toggle aria-controls="basic-navbar-nav" />
                     <Navbar.Collapse id="basic-navbar-nav">
                         <Nav className="me-auto">
-                            <Nav.Link href="#home">Home</Nav.Link>
-                            <Nav.Link href="#link">Link</Nav.Link>
-                            <NavDropdown title="Dropdown" id="basic-nav-dropdown">
-                                <NavDropdown.Item href="#action/3.1">Action</NavDropdown.Item>
-                                <NavDropdown.Item href="#action/3.2">Another action</NavDropdown.Item>
-                                <NavDropdown.Item href="#action/3.3">Something</NavDropdown.Item>
-                                <NavDropdown.Divider />
-                                <NavDropdown.Item href="#action/3.4">Separated link</NavDropdown.Item>
-                            </NavDropdown>
+                            <Nav.Link href="">Overview</Nav.Link>
+                            <Nav.Link href="#home">Download</Nav.Link>
+                            <Nav.Link href="#link">Source</Nav.Link>
                         </Nav>
                     </Navbar.Collapse>
                 </Container>
