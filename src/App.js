1c828b60b8001f63934950834452cf4f132066f0
Will Yelton <willhyelton@gmail.com>
2022-03-18 11:08:51 -0400

    Added base navbar

--- src/App.js
+++ src/App.js
@@ -1,6 +1,6 @@
 import './App.css';
 import 'bootstrap/dist/css/bootstrap.min.css';
-import {Col, Container, Row} from "react-bootstrap";
+import {Col, Container, Nav, Navbar, NavDropdown, Row} from "react-bootstrap";
 import SideBar from "./components/sidebar/SideBar";
 import {BrowserRouter, Routes, Route} from "react-router-dom";
 import ListPage from "./components/pages/ListPage";
@@ -27,23 +27,44 @@ function App() {
     });
 
     return (
-        <BrowserRouter>
-            <Routes>
-                <Route path="items" element={<ListPage title="items" data={data["items"]} sideBarData={data}/>}>
-                    <Route path=":pageID" element={<Page data={data["items"]} category="items" />} />
-                </Route>
-                <Route path="blocks" element={<ListPage title="blocks" data={data["blocks"]} sideBarData={data}/>}>
-                    <Route path=":pageID" element={<Page data={data["blocks"]} category="blocks" />} />
-                </Route>
-                <Route path="armor" element={<ListPage title="armor" data={data["armor"]} sideBarData={data}/>}>
-                    <Route path=":pageID" element={<Page data={data["armor"]} category="armor" />} />
-                </Route>
-                <Route path="tools" element={<ListPage title="tools" data={data["tools"]} sideBarData={data}/>}>
-                    <Route path=":pageID" element={<Page data={data["tools"]} category="tools" />} />
-                </Route>
-                <Route path="/" element={<HomePage sideBarData={data} />} />
-            </Routes>
-        </BrowserRouter>
+        <>
+            <Navbar bg="light" expand="lg">
+                <Container>
+                    <Navbar.Brand href="#home">React-Bootstrap</Navbar.Brand>
+                    <Navbar.Toggle aria-controls="basic-navbar-nav" />
+                    <Navbar.Collapse id="basic-navbar-nav">
+                        <Nav className="me-auto">
+                            <Nav.Link href="#home">Home</Nav.Link>
+                            <Nav.Link href="#link">Link</Nav.Link>
+                            <NavDropdown title="Dropdown" id="basic-nav-dropdown">
+                                <NavDropdown.Item href="#action/3.1">Action</NavDropdown.Item>
+                                <NavDropdown.Item href="#action/3.2">Another action</NavDropdown.Item>
+                                <NavDropdown.Item href="#action/3.3">Something</NavDropdown.Item>
+                                <NavDropdown.Divider />
+                                <NavDropdown.Item href="#action/3.4">Separated link</NavDropdown.Item>
+                            </NavDropdown>
+                        </Nav>
+                    </Navbar.Collapse>
+                </Container>
+            </Navbar>
+            <BrowserRouter>
+                <Routes>
+                    <Route path="items" element={<ListPage title="items" data={data["items"]} sideBarData={data}/>}>
+                        <Route path=":pageID" element={<Page data={data["items"]} category="items" />} />
+                    </Route>
+                    <Route path="blocks" element={<ListPage title="blocks" data={data["blocks"]} sideBarData={data}/>}>
+                        <Route path=":pageID" element={<Page data={data["blocks"]} category="blocks" />} />
+                    </Route>
+                    <Route path="armor" element={<ListPage title="armor" data={data["armor"]} sideBarData={data}/>}>
+                        <Route path=":pageID" element={<Page data={data["armor"]} category="armor" />} />
+                    </Route>
+                    <Route path="tools" element={<ListPage title="tools" data={data["tools"]} sideBarData={data}/>}>
+                        <Route path=":pageID" element={<Page data={data["tools"]} category="tools" />} />
+                    </Route>
+                    <Route path="/" element={<HomePage sideBarData={data} />} />
+                </Routes>
+            </BrowserRouter>
+        </>
     );
 }
 
